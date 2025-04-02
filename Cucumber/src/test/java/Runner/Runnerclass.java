260854255fca339573274a2382ab043b1c49c7ca
Sevvelkaran <2k21it50@kiot.ac.in>
2025-04-02 10:31:33 +0530

    commit

--- Cucumber/src/test/java/Runner/Runnerclass.java
+++ Cucumber/src/test/java/Runner/Runnerclass.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/Cucumber.html"}, features = "src/test/resources/com/features/sausedemo.feature",
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"}, features = "src/test/resources/com/features/sausedemo.feature",
 glue = "stepdefinition")
 public class Runnerclass  extends AbstractTestNGCucumberTests{
 
